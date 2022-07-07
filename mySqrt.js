8835e2280e42a96dd703fe634e6ba1ec42db16e5
Mustafa Kemal Gordesli <m.kemalgordesli@gmail.com>
2022-07-07 14:14:36 +0300

    mySqrt example added

---
+++ mySqrt.js
@@ -0,0 +1,16 @@
+// Given a non-negative integer x, compute and return the square root of x.
+// Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+// Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5
+// Input: x = 4
+// Output: 2
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+  for (let i = 0; i <= x; i++) {
+    if (i * i <= x && (i + 1) * (i + 1) > x) return i;
+  }
+};
+
+console.log(mySqrt(4));
