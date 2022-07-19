93bc682415e7dc510c11689b6ef8c757bd79495a
Mustafa Kemal Gordesli <m.kemalgordesli@gmail.com>
2022-07-19 20:24:27 +0300

    arrangeCoins example added

---
+++ arrangeCoins.js
@@ -0,0 +1,20 @@
+// You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins.
+// The last row of the staircase may be incomplete.
+// Given the integer n, return the number of complete rows of the staircase you will build.
+// Input: n = 5
+// Output: 2
+// Explanation: Because the 3rd row is incomplete, we return 2.
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var arrangeCoins = function (n) {
+  let total = 0;
+  for (let i = 0; i <= n + 1; i++) {
+    total += i;
+    if (total > n) return i - 1;
+  }
+};
+
+console.log(arrangeCoins(0));
